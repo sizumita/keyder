2a118c230abb01abbd55b9c5e8d2ce32c819f2ea
GitHub Action <action@github.com>
2023-02-08 11:16:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/sizumita/keyder/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sizumita/keyder/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sizumita.keyder.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sizumita.keyder.MyBundle
 
 class MyApplicationService {
 
